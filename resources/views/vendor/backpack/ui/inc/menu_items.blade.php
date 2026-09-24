a56cfbdd24c266ed0ec326991be81575a026adb4
Allan Miljan <allan.miljan@tptlive.ee>
2026-09-24 11:56:50 +0300

    Revert commit

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,4 +1,2 @@
 {{-- This file is used for menu items by any Backpack v7 theme --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-
-<x-backpack::menu-item title="Products" icon="la la-question" :link="backpack_url('product')" />
