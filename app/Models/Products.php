8eb2a42b954925059ac19e8262db651a16ab064e
Allan Miljan <allan.miljan@tptlive.ee>
2026-09-24 12:07:02 +0300

    Add Products model, controller and factory

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
-    //
+    /** @use HasFactory<\Database\Factories\ProductsFactory> */
+    use HasFactory;
 }
--- database/migrations/2026_09_24_090459_create_products_table.php
+++ database/migrations/2026_09_24_090459_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('sizes');
+            $table->string('colors');
             $table->timestamps();
         });
     }
